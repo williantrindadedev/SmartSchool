e6317a123f0f12b07e7e09d669df3b02ad6aac50
Willian Trindade <wtm_jo@hotmail.com>
2021-12-14 18:10:28 -0300

    Add project files.

---
+++ SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.API.Data;
+using SmartSchool.API.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartSchool.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class AlunoController : ControllerBase
+    {
+        private readonly SmartContext _context;
+        public AlunoController(SmartContext context) 
+        {
+            _context = context;
+        }
+
+        //public List<Aluno> Alunos = new List<Aluno> { 
+        //    new Aluno()
+        //    {
+        //        Id = 1,
+        //        Nome = "Willian",
+        //        Sobrenome = "Trindade",
+        //        Telefone = "(65)9 9258-2714"
+        //    },
+        //    new Aluno()
+        //    {
+        //        Id = 2,
+        //        Nome = "Jocimara",
+        //        Sobrenome = "Trindade",
+        //        Telefone = "(65)9 9272-8058"
+        //    },
+        //    new Aluno()
+        //    {
+        //        Id = 3,
+        //        Nome = "Alice",
+        //        Sobrenome = "Trindade",
+        //        Telefone = "(65)9 9212-2058"
+        //    },
+        //    new Aluno()
+        //    {
+        //        Id = 4,
+        //        Nome = "Pedro",
+        //        Sobrenome = "Trindade",
+        //        Telefone = "(65)9 9212-3333"
+        //    },
+
+        //};
+        
+        public IActionResult Get()
+        {
+            return Ok(_context.Alunos);
+        }
+
+        //[HttpGet ("{id:int}")]
+        //[HttpGet("byId")]
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("O Aluno não foi encontrado!");
+            return Ok(aluno);
+        }
+
+        //[HttpGet("{nome}")]
+        [HttpGet("byName")]
+        public IActionResult GetByName(string nome, string sobrenome)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Nome.Contains(nome) &&
+                                                     a.Sobrenome.Contains(sobrenome));
+            if (aluno == null) return BadRequest("O Aluno não foi encontrado!");
+            return Ok(aluno);
+        }
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            _context.Add(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            var alu = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if (alu == null) return BadRequest("Aluno Não encontrado!");
+
+            _context.Update(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
+        }
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            var alu = _context.Alunos.AsNoTracking().FirstOrDefault(a => a.Id == id);
+            if (alu == null) return BadRequest("Aluno Não encontrado!");
+            
+            _context.Update(aluno);
+            _context.SaveChanges();
+            return Ok(aluno);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("Aluno Inexistente!");
+
+            _context.Remove(aluno);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ SmartSchool.API/Controllers/ProfessorController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.API.Data;
+using SmartSchool.API.Models;
+using System.Linq;
+
+namespace SmartSchool.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProfessorController : ControllerBase
+    {
+        private readonly SmartContext _context;
+        public ProfessorController(SmartContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Get()
+        {
+            return Ok(_context.Professores);
+        }
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var professor = _context.Professores.FirstOrDefault(p => p.Id == id);
+            if (professor == null) return BadRequest("Professor não encontrado");
+            return Ok(professor);
+        }
+        [HttpPost]
+        public IActionResult Post(Professor professor)
+        {
+            _context.Add(professor);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id,Professor professor)
+        {
+            var prof = _context.Professores.AsNoTracking().FirstOrDefault(p => p.Id == id);
+            if (prof == null) return BadRequest("Professor não encontrado");
+
+            _context.Update(professor);
+            _context.SaveChanges();
+            return Ok();
+
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Professor professor)
+        {
+            var prof = _context.Professores.AsNoTracking().FirstOrDefault(p => p.Id == id);
+            if (prof == null) return BadRequest("Professor não encontrado");
+
+            _context.Update(professor);
+            _context.SaveChanges();
+            return Ok();
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var professor = _context.Professores.FirstOrDefault(p => p.Id == id);
+            if (professor == null) return BadRequest("Professor Inexistente!");
+            _context.Remove(professor);
+            _context.SaveChanges();
+            return Ok ();
+        }
+
+
+
+    }
+}
